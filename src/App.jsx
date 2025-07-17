09c79db63fd4a8b5eb4d8dfaaf5cb87a0ea9fb1a
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-07-18 00:57:14 +0300

    creates profile page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import ProductCategories from "./pages/ProductCategories"; //  Added import
+import UserProfileDashboard from "./pages/Profile";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/Product Categories" element={<ProductCategories />} /> {/* Added route */}
+            <Route path="/profile" element={<UserProfileDashboard />} />
           </Routes>
         </main>
 
