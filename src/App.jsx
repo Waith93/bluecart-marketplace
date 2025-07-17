5d95084c858bfa58078429a81829894b248b3446
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-07-17 23:35:29 +0300

    added route to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import ProductCategories from "./pages/ProductCategories";
+import ProductCategories from "./pages/ProductCategories"; // ✅ Added import
 
 function App() {
   return (
@@ -12,10 +12,10 @@ function App() {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
-            
+            <Route path="/categories" element={<ProductCategories />} /> {/* ✅ Added route */}
           </Routes>
-          <ProductCategories />
         </main>
+
       </div>
     </Router>
   );
