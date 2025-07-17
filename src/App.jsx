46b9d41ed7e6e3d9ad100295f4aa5f5d09303ca9
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-07-17 20:26:42 +0300

    added the ProductCategories.jsx, added axios and react-tostify packages to package.json, added theProductCategories to app.jsx and tailwind configurations to index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import React from 'react';
+import ProductCategories from './pages/ProductCategories';
 function App() {
   return (
     <div>
       <h1>Welcome to Bluecart Marketplace</h1>
+      <ProductCategories />
     </div>
   )
 }
