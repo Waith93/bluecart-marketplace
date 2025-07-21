ace34593ededbf042520d41ec703a40255144505
Chadwick Koo <chadwickkoo@outlook.com>
2025-07-21 11:46:06 +0300

    Modified SearchHistory with initial code

---
+++ server/app/routes/searchHistory.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from ..models import SearchHistory
+from database import get_db
+from ..schema import SearchHistoryCreate
+from auth import get_current_user
+from ..models import User
+
+router = APIRouter()
+
+@router.post("/search-history/")
+def save_search(data: SearchHistoryCreate, db: Session = Depends(get_db), user: User = Depends(get_current_user)):
+    search = SearchHistory(user_id=user.id, query=data.query)
+    db.add(search)
+    db.commit()
+    db.refresh(search)
+    return search
