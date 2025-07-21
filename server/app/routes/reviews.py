c7d7983c15016cc8b3b55397642229198c145849
Chadwick Koo <chadwickkoo@outlook.com>
2025-07-21 11:40:23 +0300

    Modified reviews.py with initial code

---
+++ server/app/routes/reviews.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from ..models import Review
+from ..models import User
+from ..schema import ReviewCreate
+from auth import get_current_user
+
+router = APIRouter()
+
+@router.post("/reviews/")
+def create_review(data: ReviewCreate, db: Session = Depends(get_db), user: User = Depends(get_current_user)):
+    review = Review(user_id=user.id, **data.dict())
+    db.add(review)
+    db.commit()
+    db.refresh(review)
+    return review
