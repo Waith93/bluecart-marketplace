62cf77633aaa13722b37c9561c1a6fcc6c08e89d
Esther-Sonia <esthersonia21@gmail.com>
2025-07-29 10:40:55 +0300

    update env file path in config

--- server/app/config.py
+++ server/app/config.py
@@ -1,16 +1,20 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-
-    SECRET_KEY: str 
-
+    SECRET_KEY: str
     RAPIDAPI_AMAZON_KEY: str = ""
     RAPIDAPI_AMAZON_HOST: str = ""
     RAPIDAPI_AMAZON_BASE_URL: str = ""
-    
-    
 
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    model_config = SettingsConfigDict(env_file="server/.env", extra="ignore")
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        print("SECRET_KEY:", os.getenv("SECRET_KEY"))
+        print("DATABASE_URL:", os.getenv("DATABASE_URL"))
+        print("RAPIDAPI_AMAZON_KEY:", os.getenv("RAPIDAPI_AMAZON_KEY"))
 
 settings = Settings()
+
