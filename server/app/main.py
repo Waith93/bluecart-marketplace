e7ff91fffce78e4d60e10ce9ae37f3fef09a42dc
Esther-Sonia <esthersonia21@gmail.com>
2025-07-29 11:14:57 +0300

    add frontend render link

--- server/app/main.py
+++ server/app/main.py
@@ -14,10 +14,14 @@ load_dotenv()  # Load environment variables from .env file
 
 app = FastAPI()
 
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:5173"],  
+    allow_origins=[
+        "http://localhost:3000", 
+        "http://127.0.0.1:3000", 
+        "http://localhost:5173",
+        "https://bluecart-marketplace-frontend.onrender.com" 
+    ],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
